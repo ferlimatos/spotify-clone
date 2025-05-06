25acd2d4c50a85dcf28fcb261d87b5a0f299cfaf
ferlimatos <fernandamatos.design@gmail>
2025-05-06 14:50:38 -0300

    Adicionando tela inicial (home) e organizando arquivos CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    // Aqui você poderia validar login...
+    
+    window.location.href = "home.html"; // Redireciona para a tela inicial
+  });
+  
